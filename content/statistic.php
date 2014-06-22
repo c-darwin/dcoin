8f43155ea99f08397d0e2112333d5018ce1fb058
Darwin <darwin84@yandex.ru>
2014-06-23 00:59:10 +0400

    statistic.php

--- content/statistic.php
+++ content/statistic.php
@@ -1,65 +1,50 @@
 <?php
 if (!defined('DC')) die("!defined('DC')");
 
-$res = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, '
-		SELECT *
-		FROM `'.DB_PREFIX.'daemons`
-		ORDER BY `script`
-		');
-while ($row = $db->fetchArray($res)) {
-	$row['time'] = date('d-m-Y H:i:s', $row['time']);
-	$row['memory'] = round($row['memory']/1024/1024, 2);
-	$tpl['demons'][] = $row;
+$tpl['currency_list'] = get_currency_list($db);
+
+$variables = ParseData::get_all_variables($db);
+$time = time();
+
+// получаем кол-во DC на кошельках
+$res = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
+			SELECT `currency_id`,
+						   sum(`amount`) as sum_amount
+			FROM `".DB_PREFIX."wallets`
+			GROUP BY `currency_id`
+			");
+while ( $row = $db->fetchArray( $res ) ) {
+	$sum_wallets[$row['currency_id']] = $row['sum_amount'];
 }
 
-$res = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, '
-		SELECT `'.DB_PREFIX.'nodes_ban`.`ban_start`,
-					  `'.DB_PREFIX.'nodes_ban`.`user_id`,
-					  `'.DB_PREFIX.'miners_data`.`host`,
-					  `'.DB_PREFIX.'nodes_ban`.`info`
-		FROM `'.DB_PREFIX.'nodes_ban`
-		LEFT JOIN `'.DB_PREFIX.'miners_data` ON `'.DB_PREFIX.'miners_data`.`user_id` = `'.DB_PREFIX.'nodes_ban`.`user_id`
-		ORDER BY `ban_start`
-		');
-while ($row = $db->fetchArray($res)) {
-	$row['ban_start'] = date('d-m-Y H:i:s', $row['ban_start']);
-	$tpl['nodes_ban'][] = $row;
+// получаем кол-во TDC на обещанных суммах
+$res = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
+			SELECT `currency_id`,
+						   sum(`tdc_amount`) as sum_amount
+			FROM `".DB_PREFIX."promised_amount`
+			GROUP BY `currency_id`
+			");
+while ( $row = $db->fetchArray( $res ) ) {
+	if (!isset($sum_wallets[$row['currency_id']]))
+		$sum_wallets[$row['currency_id']] = $row['sum_amount'];
+	else
+		$sum_wallets[$row['currency_id']] += $row['sum_amount'];
 }
 
-
-$res = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, '
-		SELECT *
-		FROM `'.DB_PREFIX.'nodes_connection`
-		ORDER BY `user_id`
-		');
-while ($row = $db->fetchArray($res)) {
-	$tpl['nodes_connection'][] = $row;
+// получаем суммы обещанных сумм
+$res = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
+			SELECT `currency_id`,
+						   sum(`amount`) as sum_amount
+			FROM `".DB_PREFIX."promised_amount`
+			WHERE `status` = 'mining' AND
+						 `del_block_id` = 0 AND
+						  (`cash_request_out_time` = 0 OR `cash_request_out_time` > ".($time - $variables['cash_request_time']).")
+			GROUP BY `currency_id`
+			");
+while ( $row = $db->fetchArray( $res ) ) {
+	$sum_promised_amount[$row['currency_id']] = $row['sum_amount'];
 }
 
-$res = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, '
-		SELECT *
-		FROM `'.DB_PREFIX.'main_lock`
-		');
-while ($row = $db->fetchArray($res)) {
-	$row['lock_time'] = date('d-m-Y H:i:s', $row['lock_time']);
-	$tpl['main_lock'][] = $row;
-}
-
-$tpl['queue_tx'] = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, '
-		SELECT count(*)
-		FROM `'.DB_PREFIX.'queue_tx`
-		', 'fetch_one');
-
-$tpl['transactions_testblock'] = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, '
-		SELECT count(*)
-		FROM `'.DB_PREFIX.'transactions_testblock`
-		', 'fetch_one');
-
-$tpl['transactions'] = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, '
-		SELECT count(*)
-		FROM `'.DB_PREFIX.'transactions`
-		', 'fetch_one');
-
-require_once( ABSPATH . 'templates/db_info.tpl' );
+require_once( ABSPATH . 'templates/statistic.tpl' );
 
 ?>
