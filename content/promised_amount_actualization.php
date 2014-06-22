6bd1ec69349869d9000b1520305eebfc39b142bb
Darwin <darwin84@yandex.ru>
2014-06-23 00:59:40 +0400

    promised_amount_actualization.php

--- content/promised_amount_actualization.php
+++ content/promised_amount_actualization.php
@@ -3,11 +3,11 @@ if (!defined('DC')) die("!defined('DC')");
 
 require_once( ABSPATH . 'includes/class-parsedata.php' );
 
-$tpl['data']['type'] = 'for_repaid_fix';
+$tpl['data']['type'] = 'actualization_promised_amounts';
 $tpl['data']['type_id'] = ParseData::findType($tpl['data']['type']);
 $tpl['data']['time'] = time();
 $tpl['data']['user_id'] = $user_id;
 
-require_once( ABSPATH . 'templates/for_repaid_fix.tpl' );
+require_once( ABSPATH . 'templates/promised_amount_actualization.tpl' );
 
 ?>
