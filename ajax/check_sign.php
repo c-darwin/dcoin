bedb682805f718fad659ca6a804227369acb6345
Darwin <darwin84@yandex.ru>
2014-06-23 00:52:02 +0400

    check_sign.php

--- ajax/check_sign.php
+++ ajax/check_sign.php
@@ -76,25 +76,57 @@ if ($community) {
 		}
 	}
 
-	// если дошли досюда, значит ни один ключ не подошел
+	// если дошли досюда, значит ни один ключ не подошел и нужно предложить зарегаться в пуле, если есть места
+	$pool_max_users = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
+			SELECT `pool_max_users`
+			FROM `".DB_PREFIX."config`
+			", 'fetch_one' );
+	if (sizeof($community)>=$pool_max_users)
+		die(json_encode(array('result'=>'not_available')));
+
+
 	$rsa = new Crypt_RSA();
 	$key = array();
 	$key['e'] = new Math_BigInteger($_POST['e'], 16);
 	$key['n'] = new Math_BigInteger($_POST['n'], 16);
 	$rsa->setPublicKey($key, CRYPT_RSA_PUBLIC_FORMAT_RAW);
 	$PublicKey = clear_public_key($rsa->getPublicKey());
-
+	$PublicKey_bin = hextobin($PublicKey);
+	debug_print('>$PublicKey='.$PublicKey, __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__);
 	$user_id =$db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
 				SELECT `user_id`
 				FROM `".DB_PREFIX."users`
 				WHERE `public_key_0` = 0x{$PublicKey}
 				", 'fetch_one' );
 	if ($user_id) {
-		session_start();
+		$for_sign = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
+				SELECT `data`
+				FROM `".DB_PREFIX."authorization`
+				WHERE `hash` = 0x{$hash}
+				", 'fetch_one' );
+		$error = ParseData::checkSign($PublicKey_bin, $for_sign, $sign, true);
+		if (!$error) {
+			// если юзер смог подписать наш хэш, значит у него актуальный праймари ключ
+			// и если у нас еще есть места, то создаем для него таблицы и даем войти в его новый акк
+			$mysqli_link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
+			$db_name = DB_NAME;
+			$prefix = DB_PREFIX;
+			include ABSPATH.'schema.php';
+			mysqli_query($mysqli_link, 'SET NAMES "utf8" ');
+			pool_add_users ("{$user_id};{$PublicKey}\n", $my_queries, $mysqli_link, DB_PREFIX, false);
+
+			session_start();
+			$_SESSION['user_id'] = $user_id;
+			if (!$_SESSION['user_id'])
+				$_SESSION['user_id'] = 'wait';
+			print json_encode(array('result'=>1));
+			exit;
+		}
+		/*session_start();
 		$_SESSION['user_id'] = $user_id;
 		$_SESSION['restricted'] = 1; // ограниченный режим
 		print json_encode(array('result'=>1));
-		exit;
+		exit;*/
 	}
 }
 else {
