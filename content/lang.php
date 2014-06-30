f145d0d36dbba9441ddd1b755c051db30ab57416
Darwin <darwin84@yandex.ru>
2014-06-30 21:39:53 +0400

    lang.php

--- content/lang.php
+++ content/lang.php
@@ -1,11 +1,6 @@
 <?php
 if (!defined('DC')) die("!defined('DC')");
 
-$tpl['show_sign_data'] = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, '
-		SELECT `show_sign_data`
-		FROM `'.DB_PREFIX.MY_PREFIX.'my_table`
-		', 'fetch_one');
-
-require_once( ABSPATH . 'templates/interface.tpl' );
+require_once( ABSPATH . 'templates/lang.tpl' );
 
 ?>
