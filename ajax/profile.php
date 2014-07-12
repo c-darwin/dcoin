9d311708197105dc90c5130482d4010dcfae5d1d
Darwin <darwin84@yandex.ru>
2014-07-12 12:08:19 +0400

    profile.php

--- ajax/profile.php
+++ ajax/profile.php
@@ -21,31 +21,28 @@ $user_id = $_REQUEST['user_id'];
 if ( !check_input_data ($user_id , 'int') )
 	die('error user_id');
 
-$data = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
-				SELECT `photo_block_id`,
-							 `photo_max_miner_id`,
-							 `miners_keepers`
-				FROM `".DB_PREFIX."miners_data`
-				WHERE `user_id` = {$user_id}
-				LIMIT 1
-				", 'fetch_array' );
-// получим ID майнеров, у которых лежат фото нужного нам юзера
-$miners_ids = ParseData::get_miners_keepers($data['photo_block_id'], $data['photo_max_miner_id'],  $data['miners_keepers'], true);
+$db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "SET NAMES UTF8");
+$res = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
+		SELECT *
+		FROM `".DB_PREFIX."abuses`
+		WHERE `user_id` = {$user_id}
+		");
+$abuses = '';
+while ($row = $db->fetchArray($res)) {
+	$abuses .= 'from_user_id: '.$row['from_user_id'].'; time: '.date('d-m-Y H:i:s',$row['time']).'; comment: '.$row['comment']."<br>";
+}
+if (!$abuses)
+	$abuses = 'No';
 
-// берем 1 случайный из 10-и ID майнеров
-$r = array_rand($miners_ids, 1);
-$miner_id = $miners_ids[$r];
-
-$host = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
-				SELECT `host`
-				FROM `".DB_PREFIX."miners_data`
-				WHERE `miner_id` = {$miner_id}
-				LIMIT 1
-				", 'fetch_one' );
-
+$reg_time = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
+		SELECT `reg_time`
+		FROM `".DB_PREFIX."miners_data`
+		WHERE `user_id` = {$user_id}
+		", 'fetch_one');
+$reg_time = date('d-m-Y H:i:s', $reg_time);
 echo json_encode(
-	array( 'face'=>"{$host}public/face_{$user_id}.jpg",
-			  'profile'=>"{$host}public/profile_{$user_id}.jpg"
+	array( 'abuses'=>$abuses,
+			   'reg_time'=>$reg_time
 			)
 );
 
