188521fca700a136adf88f70096377c386ea0496
Darwin <darwin84@yandex.ru>
2014-08-10 15:23:38 +0400

    content/del_cf_funding.php

--- content/del_cf_funding.php
+++ content/del_cf_funding.php
@@ -3,13 +3,13 @@ if (!defined('DC')) die("!defined('DC')");
 
 require_once( ABSPATH . 'includes/class-parsedata.php' );
 
-$tpl['data']['type'] = 'del_cf_project';
+$tpl['data']['type'] = 'del_cf_funding';
 $tpl['data']['type_id'] = ParseData::findType($tpl['data']['type']);
 $tpl['data']['time'] = time();
 $tpl['data']['user_id'] = $user_id;
 
 $tpl['del_id'] = $_REQUEST['parameters']['del_id'];
 
-require_once( ABSPATH . 'templates/del_cf_project.tpl' );
+require_once( ABSPATH . 'templates/del_cf_funding.tpl' );
 
 ?>
