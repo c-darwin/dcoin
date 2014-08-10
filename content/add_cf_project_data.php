3ce272a550b005b1d3d78e201dbcab36b1c45a14
Darwin <darwin84@yandex.ru>
2014-08-10 15:23:38 +0400

    content/add_cf_project_data.php

--- content/add_cf_project_data.php
+++ content/add_cf_project_data.php
@@ -1,13 +1,34 @@
 <?php
 if (!defined('DC')) die("!defined('DC')");
 
-$tpl['data']['type'] = 'new_cf_project';
+$tpl['data']['type'] = 'cf_project_data';
 $tpl['data']['type_id'] = ParseData::findType($tpl['data']['type']);
 $tpl['data']['time'] = time();
 $tpl['data']['user_id'] = $user_id;
 
-$tpl['variables'] = ParseData::get_variables ($db,  array('limit_change_host', 'limit_change_host_period') );
+// если есть project_id, значит это добавление новго описания на новом языке
+$tpl['project_id'] = intval($_REQUEST['parameters']['project_id']);
 
-require_once( ABSPATH . 'templates/new_cf_project.tpl' );
+// если есть id, значит юзер нажал "редактировать описание"
+$tpl['id'] = intval($_REQUEST['parameters']['id']);
+
+if ($tpl['id']) {
+	$tpl['cf_data'] = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
+			SELECT *
+			FROM `".DB_PREFIX."cf_projects_data`
+			WHERE `id` = {$tpl['id']}
+			", 'fetch_array');
+	$tpl['project_id'] = $tpl['cf_data'] ['project_id'];
+}
+
+$tpl['cf_currency_name'] = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
+		SELECT `project_currency_name`
+		FROM `".DB_PREFIX."cf_projects`
+		WHERE `id` = {$tpl['project_id']}
+		", 'fetch_one');
+
+$tpl['cf_lng'] = get_all_cf_lng($db);
+
+require_once( ABSPATH . 'templates/add_cf_project_data.tpl' );
 
 ?>
