0a6f75630a2aa2a10bf418d61839185629a2cf77
Darwin <darwin84@yandex.ru>
2014-08-10 15:23:38 +0400

    content/cf_catalog.php

--- content/cf_catalog.php
+++ content/cf_catalog.php
@@ -1,13 +1,26 @@
 <?php
 if (!defined('DC')) die("!defined('DC')");
 
-$tpl['data']['type'] = 'cf_comment';
-$tpl['data']['type_id'] = ParseData::findType($tpl['data']['type']);
-$tpl['data']['time'] = time();
-$tpl['data']['user_id'] = $user_id;
+$tpl['category_id'] = intval($_REQUEST['parameters']['category_id']);
+$add_sql = '';
+if ($tpl['category_id'])
+	$add_sql = " AND `category_id` = '{$tpl['category_id']}' ";
 
-$tpl['project_id'] = $_REQUEST['parameters']['project_id'];
+$res = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
+		SELECT *
+		FROM `".DB_PREFIX."cf_projects`
+		WHERE `del_block_id`=0
+				 	  {$add_sql}
+		");
+while ( $row =  $db->fetchArray( $res ) ) {
 
-require_once( ABSPATH . 'templates/add_cf_comment.tpl' );
+	$row = array_merge (project_data($row), $row);
+	$tpl['projects'][$row['id']] = $row;
+}
+
+
+asort($lng['cf_category']);
+
+require_once( ABSPATH . 'templates/cf_catalog.tpl' );
 
 ?>
