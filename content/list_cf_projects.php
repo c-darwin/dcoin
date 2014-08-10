b80ec30d832a2e3ef811ca3bc96b5ea3f0fde256
Darwin <darwin84@yandex.ru>
2014-08-10 15:23:38 +0400

    content/list_cf_projects.php

--- content/list_cf_projects.php
+++ content/list_cf_projects.php
@@ -1,13 +1,6 @@
 <?php
 if (!defined('DC')) die("!defined('DC')");
 
-$tpl['data']['type'] = 'new_cf_project';
-$tpl['data']['type_id'] = ParseData::findType($tpl['data']['type']);
-$tpl['data']['time'] = time();
-$tpl['data']['user_id'] = $user_id;
-
-$tpl['variables'] = ParseData::get_variables ($db,  array('limit_change_host', 'limit_change_host_period') );
-
-require_once( ABSPATH . 'templates/new_cf_project.tpl' );
+require_once( ABSPATH . 'templates/list_cf_projects.tpl' );
 
 ?>
