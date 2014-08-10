6397611257c73d9c44d1748dd8e05cba2b24fce6
Darwin <darwin84@yandex.ru>
2014-08-10 15:23:38 +0400

    content/my_cf_projects.php

--- content/my_cf_projects.php
+++ content/my_cf_projects.php
@@ -1,6 +1,21 @@
 <?php
 if (!defined('DC')) die("!defined('DC')");
 
-require_once( ABSPATH . 'templates/list_cf_projects.tpl' );
 
+$res = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
+		SELECT *
+		FROM `".DB_PREFIX."cf_projects`
+		WHERE `user_id` = {$user_id} AND
+					 `del_block_id`=0
+		");
+while ( $row =  $db->fetchArray( $res ) ) {
+
+	$row = array_merge (project_data($row), $row);
+
+	$tpl['projects'][$row['id']] = $row;
+}
+
+$tpl['cf_lng'] = get_all_cf_lng($db);
+
+require_once( ABSPATH . 'templates/my_cf_projects.tpl' );
 ?>
