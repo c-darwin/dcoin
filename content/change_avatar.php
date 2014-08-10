9b6cb9d047522e89fdd148702cb79abcb87c2d18
Darwin <darwin84@yandex.ru>
2014-08-10 15:23:38 +0400

    content/change_avatar.php

--- content/change_avatar.php
+++ content/change_avatar.php
@@ -1,13 +1,19 @@
 <?php
 if (!defined('DC')) die("!defined('DC')");
 
-$tpl['data']['type'] = 'cf_comment';
+$tpl['data']['type'] = 'user_avatar';
 $tpl['data']['type_id'] = ParseData::findType($tpl['data']['type']);
 $tpl['data']['time'] = time();
 $tpl['data']['user_id'] = $user_id;
 
-$tpl['project_id'] = $_REQUEST['parameters']['project_id'];
+$data = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
+		SELECT `name`,
+					 `avatar`
+		FROM `".DB_PREFIX."users`
+		WHERE `user_id`= {$user_id}
+		", 'fetch_array');
+$tpl = array_merge($tpl, $data);
 
-require_once( ABSPATH . 'templates/add_cf_comment.tpl' );
+require_once( ABSPATH . 'templates/change_avatar.tpl' );
 
 ?>
