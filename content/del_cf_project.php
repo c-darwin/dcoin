1bf082c85e487a9d3195d1c5a7de0d433371260d
Darwin <darwin84@yandex.ru>
2014-08-10 15:23:38 +0400

    content/del_cf_project.php

--- content/del_cf_project.php
+++ content/del_cf_project.php
@@ -3,13 +3,18 @@ if (!defined('DC')) die("!defined('DC')");
 
 require_once( ABSPATH . 'includes/class-parsedata.php' );
 
-$tpl['data']['type'] = 'del_promised_amount';
+$tpl['data']['type'] = 'del_cf_project';
 $tpl['data']['type_id'] = ParseData::findType($tpl['data']['type']);
 $tpl['data']['time'] = time();
 $tpl['data']['user_id'] = $user_id;
 
-$tpl['del_id'] = $_REQUEST['parameters']['del_id'];
+$tpl['del_id'] = intval($_REQUEST['parameters']['del_id']);
+$tpl['project_currency_name'] = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
+		SELECT `project_currency_name`
+		FROM `".DB_PREFIX."cf_projects`
+		WHERE `id`= {$tpl['del_id']}
+		", 'fetch_one');
 
-require_once( ABSPATH . 'templates/promised_amount_delete.tpl' );
+require_once( ABSPATH . 'templates/del_cf_project.tpl' );
 
 ?>
