81e10443519bcbdeef3e65aae0838cb013e1289f
Darwin <darwin84@yandex.ru>
2014-08-13 00:18:17 +0400

    get_block.php

--- get_block.php
+++ get_block.php
@@ -14,7 +14,12 @@ require_once( ABSPATH . 'includes/class-mysql.php' );
 $db = new MySQLidb(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
 
 if (check_input_data($_REQUEST['id'], 'int') ) {
-		
+
+	if (isset($_REQUEST['download'])) {
+		header('Content-type: application/octet-stream');
+		header('Content-Disposition: attachment; filename="'.$_REQUEST['id'].'"');
+	}
+
 	$block = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
 			SELECT `data`
 			FROM `".DB_PREFIX."block_chain`
