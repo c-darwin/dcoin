72878b9be22a5453c58ad70af61092a583e8fa85
Darwin <darwin84@yandex.ru>
2014-08-14 19:34:22 +0400

    content/login.php

--- content/login.php
+++ content/login.php
@@ -1,8 +1,16 @@
 <?php
 if (!defined('DC')) die("!defined('DC')");
 
-
-
+// проверим, не идут ли тех. работы на пуле
+$config = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, '
+		SELECT `pool_admin_user_id`,
+					  `pool_tech_works`
+		FROM `'.DB_PREFIX.'config`
+		', 'fetch_array');
+if ($config['pool_admin_user_id'] && $config['pool_admin_user_id']!=$_SESSION['user_id'] && $config['pool_tech_works']==1)
+	$tpl['pool_tech_works'] = 1;
+else
+	$tpl['pool_tech_works'] = 0;
 
 require_once( ABSPATH . 'templates/login.tpl' );
 
