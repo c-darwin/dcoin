63c01405f69623e1512d6ae06be5bc89c55b8ea6
Darwin <darwin84@yandex.ru>
2014-08-14 19:34:22 +0400

    ajax/pool_add_users.php

--- ajax/pool_add_users.php
+++ ajax/pool_add_users.php
@@ -8,22 +8,44 @@ define( 'DC', TRUE);
 
 define( 'ABSPATH', dirname(dirname(__FILE__)) . '/' );
 
+set_time_limit(0);
+
 //require_once( ABSPATH . 'includes/errors.php' );
 require_once( ABSPATH . 'includes/fns-main.php' );
+
 require_once( ABSPATH . 'db_config.php' );
 require_once( ABSPATH . 'includes/class-mysql.php' );
 
-$mysqli_link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
 $db = new MySQLidb(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
 
 if (!node_admin_access($db))
-	die('Permission denied');
+	die ('Permission denied');
 
-$db_name = DB_NAME;
-$prefix = DB_PREFIX;
-include ABSPATH.'schema.php';
+copy ($_FILES['file']['tmp_name'], '/tmp/111');
+$command="mysql --default-character-set=binary -h ".DB_HOST." -u '".DB_USER."' -p'".DB_PASSWORD."'  '".DB_NAME."' < '{$_FILES['file']['tmp_name']}'";
+$res = shell_exec($command);
 
-$error = pool_add_users ($_POST['pool_data'], $my_queries, $mysqli_link, DB_PREFIX);
-if ($error)
-	print $error;
+// теперь нужно заполнить таблу cimmunity
+
+$tables_array = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
+			SHOW TABLES
+			", 'array');
+$arr = array();
+foreach($tables_array as $table) {
+	if (preg_match('/([0-9]+)_my/i', $table, $m)) {
+		$db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__,  "
+			INSERT IGNORE INTO `".DB_PREFIX."community` (
+				`user_id`
+			)
+			VALUES (
+				{$m[1]}
+			)");
+	}
+}
+
+if ($_FILES['file']['error']>0)
+	echo json_encode(array('error'=>'error. code '.$_FILES['file']['error']));
+else
+	echo json_encode(array('success'=>'ok'));
+
 ?>
