30aa22e0aaba50fd3f2a3a70489550b56e5317ad
Darwin <darwin84@yandex.ru>
2014-08-16 22:50:36 +0400

    index.php

--- index.php
+++ index.php
@@ -14,17 +14,7 @@ require_once( ABSPATH . 'includes/fns-main.php' );
 //require_once( ABSPATH . 'db_config.php' );
 require_once( ABSPATH . 'includes/class-mysql.php' );
 
-if (!isset($lang)) {
-	if (@$_SESSION['lang'])
-		$lang = $_SESSION['lang'];
-	else if (@$_COOKIE['lang'])
-		$lang = $_COOKIE['lang'];
-}
-if (!@$lang)
-	$lang = $default_lang;
-
-if (!preg_match('/^[a-z]{2}$/iD', $lang))
-	die('lang error');
+$lang = get_lang();
 require_once( ABSPATH . 'lang/'.$lang.'.php' );
 
 //$db = new MySQLidb(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
