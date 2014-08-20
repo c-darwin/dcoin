15ca333bdd335fd128a100b5270ec203e5a4e6b3
root <you@example.com>
2014-08-20 19:19:19 +0400

    ajax/sign_up_in_pool.php

--- ajax/sign_up_in_pool.php
+++ ajax/sign_up_in_pool.php
@@ -3,6 +3,7 @@ session_start();
 
 if ( empty($_SESSION['user_id']) )
 	die('!user_id');
+$user_id = intval($_SESSION['user_id']);
 
 define( 'DC', TRUE);
 
@@ -12,40 +13,82 @@ set_time_limit(0);
 
 //require_once( ABSPATH . 'includes/errors.php' );
 require_once( ABSPATH . 'includes/fns-main.php' );
-
 require_once( ABSPATH . 'db_config.php' );
 require_once( ABSPATH . 'includes/class-mysql.php' );
+require_once( ABSPATH . 'phpseclib/Math/BigInteger.php');
+require_once( ABSPATH . 'phpseclib/Crypt/Random.php');
+require_once( ABSPATH . 'phpseclib/Crypt/Hash.php');
+require_once( ABSPATH . 'phpseclib/Crypt/RSA.php');
+require_once( ABSPATH . 'phpseclib/Crypt/AES.php');
 
 $db = new MySQLidb(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
 
-if (!node_admin_access($db))
-	die ('Permission denied');
+$email = $_REQUEST['email'];
+if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+	die(json_encode(array('error'=>'incorrect email')));
 
-copy ($_FILES['file']['tmp_name'], '/tmp/111');
-$command="mysql --default-character-set=binary -h ".DB_HOST." -u '".DB_USER."' -p'".DB_PASSWORD."'  '".DB_NAME."' < '{$_FILES['file']['tmp_name']}'";
-$res = shell_exec($command);
 
-// теперь нужно заполнить таблу cimmunity
+$lang = get_lang();
+require_once( ABSPATH . 'lang/'.$lang.'.php' );
 
-$tables_array = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
-			SHOW TABLES
-			", 'array');
-$arr = array();
-foreach($tables_array as $table) {
-	if (preg_match('/([0-9]+)_my/i', $table, $m)) {
-		$db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__,  "
-			INSERT IGNORE INTO `".DB_PREFIX."community` (
+// если мест в пуле нет, то просто запишем юзера в очередь
+$pool_max_users = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
+			SELECT `pool_max_users`
+			FROM `".DB_PREFIX."config`
+			", 'fetch_one' );
+if (sizeof($community) >= $pool_max_users) {
+	$db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__,  "
+			INSERT IGNORE INTO `".DB_PREFIX."pool_waiting_list` (
+				`email`,
+				`time`,
 				`user_id`
 			)
 			VALUES (
-				{$m[1]}
+					'{$email}',
+					".time().",
+					{$user_id}
 			)");
-	}
+	die(json_encode(array('error'=>'К сожалению, пул переполнен. Мы сообщим Вам, когда будет запущен новый пул.')));
 }
 
-if ($_FILES['file']['error']>0)
-	echo json_encode(array('error'=>'error. code '.$_FILES['file']['error']));
-else
-	echo json_encode(array('success'=>'ok'));
+// регистрируем юзера в пуле
+// вначале убедитмся, что такой user_id у нас уже не зареган
+$community = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
+		SELECT `user_id`
+		FROM `".DB_PREFIX."community`
+		WHERE `user_id` = {$user_id}
+		", 'fetch_one' );
+if ($community) {
+	die(json_encode(array('error'=>'Ваш user_id уже зарегистрирован в нашем пуле. Пожалуйста, свяжитесь с администрацией.')));
+}
 
+$db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__,  "
+		INSERT IGNORE INTO `".DB_PREFIX."community` (
+			`user_id`
+		)
+		VALUES (
+			{$user_id}
+		)");
+
+$rsa = new Crypt_RSA();
+$key = array();
+$key['e'] = new Math_BigInteger($_POST['e'], 16);
+$key['n'] = new Math_BigInteger($_POST['n'], 16);
+$rsa->setPublicKey($key, CRYPT_RSA_PUBLIC_FORMAT_RAW);
+$PublicKey = clear_public_key($rsa->getPublicKey());
+
+// если таблы my для этого юзера уже есть в БД, то они перезапишутся.
+$mysqli_link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
+$db_name = DB_NAME;
+$prefix = DB_PREFIX;
+include ABSPATH.'schema.php';
+mysqli_query($mysqli_link, 'SET NAMES "utf8" ');
+pool_add_users ("{$user_id};{$PublicKey}\n", $my_queries, $mysqli_link, DB_PREFIX, false);
+
+define('MY_PREFIX', $user_id.'_');
+$db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
+		UPDATE `".DB_PREFIX.MY_PREFIX."my_table`
+		SET `email` = '{$email}'
+		");
+print json_encode(array('success'=>'Поздравляем, теперь Вы зарегистрированы на нашем пуле. Обновите страницу и заново введите свой ключ'));
 ?>
