0140d71622dc2b76924cf8b66969f8d7ab2b743d
root <you@example.com>
2014-08-20 19:19:19 +0400

    ajax/check_sign.php

--- ajax/check_sign.php
+++ ajax/check_sign.php
@@ -40,8 +40,6 @@ if ($community) {
 				FROM `".DB_PREFIX."{$my_prefix}my_keys`
 				WHERE `block_id` = (SELECT max(`block_id`) FROM `".DB_PREFIX."{$my_prefix}my_keys` )
 				", 'fetch_one' );
-		//print $public_key;
-		//print $db->printsql()."\n";
 
 		$ini_array = parse_ini_file(ABSPATH . "config.ini", true);
 		//print ($ini_array['main']['sign_hash'])."\n";
@@ -80,15 +78,16 @@ if ($community) {
 		}
 	}
 
-	// если дошли досюда, значит ни один ключ не подошел и нужно предложить зарегаться в пуле, если есть места
+	// если дошли досюда, значит ни один ключ не подошел и даем возможность войти в ограниченном режиме
+
+/*
 	$pool_max_users = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
 			SELECT `pool_max_users`
 			FROM `".DB_PREFIX."config`
 			", 'fetch_one' );
 	if (sizeof($community)>=$pool_max_users)
 		die(json_encode(array('result'=>'not_available')));
-
-
+*/
 	$rsa = new Crypt_RSA();
 	$key = array();
 	$key['e'] = new Math_BigInteger($_POST['e'], 16);
@@ -112,25 +111,20 @@ if ($community) {
 		if (!$error) {
 			// если юзер смог подписать наш хэш, значит у него актуальный праймари ключ
 			// и если у нас еще есть места, то создаем для него таблицы и даем войти в его новый акк
+			/*
 			$mysqli_link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
 			$db_name = DB_NAME;
 			$prefix = DB_PREFIX;
 			include ABSPATH.'schema.php';
 			mysqli_query($mysqli_link, 'SET NAMES "utf8" ');
 			pool_add_users ("{$user_id};{$PublicKey}\n", $my_queries, $mysqli_link, DB_PREFIX, false);
-
+			*/
 			session_start();
 			$_SESSION['user_id'] = $user_id;
-			if (!$_SESSION['user_id'])
-				$_SESSION['user_id'] = 'wait';
+			$_SESSION['restricted'] = 1;
 			print json_encode(array('result'=>1));
 			exit;
 		}
-		/*session_start();
-		$_SESSION['user_id'] = $user_id;
-		$_SESSION['restricted'] = 1; // ограниченный режим
-		print json_encode(array('result'=>1));
-		exit;*/
 	}
 }
 else {
