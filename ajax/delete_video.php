d7002ed7c7968a40af9b2d7bd1d9930b21c81d2d
root <you@example.com>
2014-08-22 11:20:47 +0400

    ajax/delete_video.php

--- ajax/delete_video.php
+++ ajax/delete_video.php
@@ -15,15 +15,15 @@ require_once( ABSPATH . 'includes/class-mysql.php' );
 
 $db = new MySQLidb(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
 
-if (empty($_SESSION['restricted']) && !get_community_users($db)) {
+if (!node_admin_access($db))
+	die ('Permission denied');
 
-	if ( $_REQUEST['type'] == 'mp4' )
-		@unlink( ABSPATH . 'public/'.$_SESSION['user_id'].'_user_video.mp4' );
+if ( $_REQUEST['type'] == 'mp4' )
+	@unlink( ABSPATH . 'public/'.$_SESSION['user_id'].'_user_video.mp4' );
 
-	if ( $_REQUEST['type'] == 'webm_ogg' ) {
-		@unlink( ABSPATH . 'public/'.$_SESSION['user_id'].'_user_video.ogv' );
-		@unlink( ABSPATH . 'public/'.$_SESSION['user_id'].'_user_video.webm' );
-	}
+if ( $_REQUEST['type'] == 'webm_ogg' ) {
+	@unlink( ABSPATH . 'public/'.$_SESSION['user_id'].'_user_video.ogv' );
+	@unlink( ABSPATH . 'public/'.$_SESSION['user_id'].'_user_video.webm' );
 }
 
 ?>
