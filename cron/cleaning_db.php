fe8fe55e8a69bad2763e3d80fc7868694725ce15
Darwin <darwin84@yandex.ru>
2014-08-22 22:26:57 +0400

    cron/cleaning_db.php

--- cron/cleaning_db.php
+++ cron/cleaning_db.php
@@ -13,15 +13,20 @@ require_once( ABSPATH . 'includes/class-mysql.php' );
 
 $db = new MySQLidb(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
 
-define('WAIT_LOCK', 3600);
+$auto_reload = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
+		SELECT `auto_reload`
+		FROM `".DB_PREFIX."config`
+		", 'fetch_one');
+if ($auto_reload < 60)
+	exit;
 
 // если main_lock висит более x минут, значит был какой-то сбой
 $main_lock = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
 		SELECT `lock_time`
 		FROM `".DB_PREFIX."main_lock`
-		WHERE `script_name` != 'my_lock'
+		WHERE `script_name` NOT IN ('my_lock', 'cleaning_db')
 		", 'fetch_one');
-if ($main_lock && (time()-WAIT_LOCK > $main_lock) ) {
+if ( $main_lock && (time() - $auto_reload > $main_lock) ) {
 
 	// на всякий случай пометим, что работаем
 	$db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
@@ -42,9 +47,16 @@ if ($main_lock && (time()-WAIT_LOCK > $main_lock) ) {
 		//if (!in_array($table, $exceptions))
 
 		if (!preg_match('/(my_|install|config|daemons|payment_systems|community|cf_lang)/i', $table)) {
+
 			$db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__,"
 					TRUNCATE TABLE `".DB_PREFIX."{$table}`
 					");
+
+			if ($table == 'cf_currency')
+				$db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__,"
+						ALTER TABLE `".DB_PREFIX."cf_currency` auto_increment = 1000
+						");
+
 		}
 	}
 }
