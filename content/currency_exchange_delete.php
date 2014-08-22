9660d8df311fb16ba2694ad3d339f0d955d9b245
root <you@example.com>
2014-08-22 11:20:47 +0400

    content/currency_exchange_delete.php

--- content/currency_exchange_delete.php
+++ content/currency_exchange_delete.php
@@ -8,7 +8,7 @@ $tpl['data']['type_id'] = ParseData::findType($tpl['data']['type']);
 $tpl['data']['time'] = time();
 $tpl['data']['user_id'] = $user_id;
 
-$tpl['del_id'] = $_REQUEST['parameters']['del_id'];
+$tpl['del_id'] = inrval($_REQUEST['parameters']['del_id']);
 
 require_once( ABSPATH . 'templates/currency_exchange_delete.tpl' );
 
