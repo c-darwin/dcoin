ca7722c699050b26ab58641f43304dd147aa78b2
root <you@example.com>
2014-08-22 11:20:47 +0400

    content/del_cf_funding.php

--- content/del_cf_funding.php
+++ content/del_cf_funding.php
@@ -8,7 +8,7 @@ $tpl['data']['type_id'] = ParseData::findType($tpl['data']['type']);
 $tpl['data']['time'] = time();
 $tpl['data']['user_id'] = $user_id;
 
-$tpl['del_id'] = $_REQUEST['parameters']['del_id'];
+$tpl['del_id'] = intval($_REQUEST['parameters']['del_id']);
 
 require_once( ABSPATH . 'templates/del_cf_funding.tpl' );
 
