72c98ce5e894444e82d6c615681607cec200dc1c
root <you@example.com>
2014-08-22 11:20:47 +0400

    ajax/save_notifications.php

--- ajax/save_notifications.php
+++ ajax/save_notifications.php
@@ -13,26 +13,28 @@ require_once( ABSPATH . 'includes/fns-main.php' );
 require_once( ABSPATH . 'db_config.php' );
 require_once( ABSPATH . 'includes/class-mysql.php' );
 
-if (empty($_SESSION['restricted'])) {
 
-	$db = new MySQLidb(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
+if (!empty($_SESSION['restricted']))
+	die('Permission denied');
 
-	define('MY_PREFIX', get_my_prefix($db));
+$db = new MySQLidb(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
+
+define('MY_PREFIX', get_my_prefix($db));
 
 	$_REQUEST['data'] = $db->escape($_REQUEST['data']);
 	$_REQUEST['data'] = str_replace('\"', '"', $_REQUEST['data']);
 	$data = json_decode($_REQUEST['data']);
 
 	for ($i=0; $i<sizeof($data); $i++ ) {
+		$data[$i]->name = $db->escape($data[$i]->name);
 		$db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
 				UPDATE `".DB_PREFIX.MY_PREFIX."my_notifications`
-				SET `email` = {$data[$i]->email},
-					   `sms` = {$data[$i]->sms}
+				SET `email` = ".intval($data[$i]->email).",
+					   `sms` = ".intval($data[$i]->sms)."
 			    WHERE `name` = '{$data[$i]->name}'
 				");
 		//print $db->printsql()."\n";
 	}
 	print '{"error":0}';
-}
 
 ?>
