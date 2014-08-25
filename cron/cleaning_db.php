6860515b415097b4da0f21e372f16984e7758518
Darwin <darwin84@yandex.ru>
2014-08-25 19:49:50 +0400

    cron/cleaning_db.php

--- cron/cleaning_db.php
+++ cron/cleaning_db.php
@@ -10,8 +10,30 @@ require_once( ABSPATH . 'includes/errors.php' );
 require_once( ABSPATH . 'includes/fns-main.php' );
 require_once( ABSPATH . 'db_config.php' );
 require_once( ABSPATH . 'includes/class-mysql.php' );
+require_once( ABSPATH . 'includes/class-parsedata.php' );
 
 $db = new MySQLidb(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
+
+// чтобы этот скрипт вызывался раз в 5 минут
+upd_deamon_time($db);
+
+// пишем свежие блоки в резервный блокчейн
+$end_block_id = get_end_block_id();
+$cur_block_id = get_block_id($db);
+print $end_block_id.' / '.$cur_block_id;
+if ($cur_block_id - 30 > $end_block_id) {
+	$res = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, '
+			SELECT `id`, `data`
+			FROM `'.DB_PREFIX.'block_chain`
+			WHERE `id` > '.$end_block_id.' AND `id` <= '.($cur_block_id - 30).'
+			ORDER BY `id`
+			');
+	while ($row = $db->fetchArray($res)) {
+		$data = dec_binary($row['id'], 5).ParseData::encode_length_plus_data($row['data']);
+		$size_and_data = dec_binary(strlen($data), 5).$data;
+		file_put_contents(ABSPATH . 'public/blockchain', $size_and_data . dec_binary(strlen($size_and_data), 5), FILE_APPEND | LOCK_EX);
+	}
+}
 
 $auto_reload = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
 		SELECT `auto_reload`
