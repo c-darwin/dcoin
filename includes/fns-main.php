d344acc6cbcbae224407862331ee4ace3f4448f3
Darwin <darwin84@yandex.ru>
2014-08-25 19:49:50 +0400

    includes/fns-main.php

--- includes/fns-main.php
+++ includes/fns-main.php
@@ -13,7 +13,7 @@ date_default_timezone_set('America/New_York');
 ##########################################################################
 $my_lock = false;
 $lock_time = 0;
-$default_lang = 'ru';
+$default_lang = '42';
 
 if(substr(PHP_OS, 0, 3) == "WIN")
 	define('OS', 'WIN');
@@ -951,7 +951,7 @@ function get_all_cf_lng($db)
 	return $lang;
 }
 
-function get_config()
+function get_node_config()
 {
 	global $db;
 	return $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
@@ -3306,6 +3306,12 @@ function clear_incompatible_tx($binary_tx, $db, $my_tx)
 		if ($type == ParseData::findType('cash_request_out'))
 			clear_incompatible_tx_sql ($db, 'new_holidays', $to_user_id, $wait_error);
 
+		// не должно попадать в одни блок new_miner_update и new_miner
+		if ($type == ParseData::findType('new_miner'))
+			clear_incompatible_tx_sql ($db, 'new_miner_update', $user_id, $wait_error);
+		if ($type == ParseData::findType('new_miner_update'))
+			clear_incompatible_tx_sql ($db, 'new_miner', $user_id, $wait_error);
+
 		// не должно попадать в один блок смена нодовского ключа и тр-ии которые этим ключем подписываются
 		if ($type == ParseData::findType('change_node_key'))
 			clear_incompatible_tx_sql ($db, 'new_miner_update', $user_id, $wait_error);
@@ -3926,6 +3932,130 @@ function get_install_progress()
 			", 'fetch_one');
 }
 
+
+function get_end_block_id()
+{
+	if (!file_exists(ABSPATH . 'public/blockchain'))
+		return 0;
+	else {
+		$rs = fopen(ABSPATH . 'public/blockchain', 'r');
+		// размер блока, записанный в 5-и последних байтах файла blockchain
+		fseek($rs,-5,SEEK_END);
+		$size = binary_dec(fread($rs, 5));
+		// сам блок
+		fseek($rs, -($size+5),SEEK_END);
+		$data_binary = fread($rs, $size+5);
+		// размер (id блока + тело блока)
+		$data_length =  binary_dec(string_shift($data_binary, 5));
+		$block_id = binary_dec(string_shift($data_binary, 5));
+		fclose($rs);
+		return $block_id;
+	}
+}
+
+function get_promised_amounts($user_id)
+{
+	global $db, $tpl, $lng;
+	// то, что уже в блоках
+	$res = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
+		SELECT *
+		FROM `".DB_PREFIX."promised_amount`
+		WHERE `user_id` = {$user_id}
+		");
+	while ($row = $db->fetchArray($res)) {
+
+		// есть ли просроченные запросы
+		$cash_request_pending = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
+				SELECT `status`
+				FROM `".DB_PREFIX."cash_requests`
+				WHERE `to_user_id` = {$user_id} AND
+							 `del_block_id` = 0 AND
+							 `for_repaid_del_block_id` = 0 AND
+							 `time` < ".(time() - $tpl['variables']['cash_request_time'])." AND
+							 `status` = 'pending'
+				LIMIT 1
+				", 'fetch_one' );
+		if ($cash_request_pending && $row['currency_id'] > 1 && $row['status'] == 'mining') {
+
+			$row['status'] = 'for_repaid';
+
+			// и заодно проверим, можно ли делать актуализацию обещанных сумм
+			$tpl['actualization_promised_amounts'] = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
+				SELECT `id`
+				FROM `".DB_PREFIX."promised_amount`
+				WHERE `status` = 'mining' AND
+							 `user_id` = {$user_id} AND
+							 `currency_id` > 1 AND
+							 `del_block_id` = 0 AND
+							 `del_mining_block_id` = 0 AND
+							 (`cash_request_out_time` > 0 AND `cash_request_out_time` < ".(time() - $tpl['variables']['cash_request_time']).")
+				", 'fetch_one');
+		}
+
+		$row['tdc'] = $row['tdc_amount'];
+		if ($row['del_block_id'])
+			continue;
+		if ($row['status']=='mining') {
+			$row['tdc']+= calc_profit_($row['currency_id'], $row['amount']+$row['tdc_amount'], $user_id, $db, $row['tdc_amount_update'], time(), 'mining');
+			$row['tdc'] = floor($row['tdc']*100)/100;
+		}
+		else if ($row['status']=='repaid') {
+			$row['tdc'] = $row['tdc_amount'] + calc_profit_($row['currency_id'], $row['tdc_amount'], $user_id, $db, $row['tdc_amount_update'], time(), 'repaid');
+			$row['tdc'] = floor($row['tdc']*100)/100;
+		}
+		else
+			$row['tdc'] = $row['tdc_amount'];
+
+		$row['status_text'] = $lng['status_'.$row['status']];
+
+		$row['max_amount'] = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
+				SELECT `amount`
+				FROM `".DB_PREFIX."max_promised_amounts`
+				WHERE `currency_id` = {$row['currency_id']}
+				ORDER BY `block_id` DESC
+				LIMIT 1
+				", 'fetch_one');
+
+		$row['max_other_currencies'] = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
+				SELECT `max_other_currencies`
+				FROM `".DB_PREFIX."currency`
+				WHERE `id` = {$row['currency_id']}
+				LIMIT 1
+				", 'fetch_one');
+
+		// для WOC amount не учитывается. Вместо него берется max_promised_amount
+		if ($row['currency_id'] == 1) {
+			$row['amount'] = $row['max_amount'];
+		}
+		if ($row['status'] == 'repaid') {
+			$row['amount'] = 0;
+		}
+
+		// последний статус юзера
+		$pct_status = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
+				SELECT `status`
+				FROM `".DB_PREFIX."points_status`
+				WHERE `user_id` = {$user_id}
+				ORDER BY `time_start` DESC
+				LIMIT 1
+				", 'fetch_one');
+
+		if (!$pct_status)
+			$pct_status = 'user';
+		$pct = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
+				SELECT `{$pct_status}`
+				FROM `".DB_PREFIX."pct`
+				WHERE `currency_id` = {$row['currency_id']}
+				ORDER BY `block_id` DESC
+				LIMIT 1
+				", 'fetch_one');
+		$row['pct'] = round((pow(1+$pct, 3600*24*365)-1)*100, 2);
+
+		// тут accepted значит просто попало в блок
+		$tpl['promised_amount_list']['accepted'][] = $row;
+
+	}
+}
 
 $my_tables = array('my_admin_messages','my_cash_requests','my_comments','my_commission','my_complex_votes','my_dc_transactions','my_holidays','my_keys','my_new_users','my_node_keys','my_notifications','my_promised_amount','my_table','my_tasks', 'my_cf_funding');
 
