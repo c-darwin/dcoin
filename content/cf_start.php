ca4fb475c62bb159e1510b5be9a14535ea4bc3c1
Darwin <darwin84@yandex.ru>
2014-09-03 12:54:57 +0400

    ajax/get_stats.php

---
+++ content/cf_start.php
@@ -0,0 +1,38 @@
+<?php
+if (!defined('DC')) die("!defined('DC')");
+
+if (!isset($tpl['cf_url'])) $tpl['cf_url'] = '';
+
+$tpl['category_id'] = intval($_REQUEST['parameters']['category_id']);
+$add_sql = '';
+if ($tpl['category_id'])
+	$add_sql = " AND `category_id` = '{$tpl['category_id']}' ";
+
+$res = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
+		SELECT `".DB_PREFIX."cf_projects`.*
+		FROM `".DB_PREFIX."cf_projects`
+		LEFT JOIN `".DB_PREFIX."cf_projects_data` ON  `".DB_PREFIX."cf_projects_data`.`project_id` = `".DB_PREFIX."cf_projects`.`id`
+		WHERE `del_block_id` = 0 AND
+					 `end_time` > ".time()." AND
+					 `lang_id` = {$lang}
+				 	  {$add_sql}
+		ORDER BY `funders` DESC
+		LIMIT 100
+		");
+while ( $row =  $db->fetchArray( $res ) ) {
+	$row = array_merge (project_data($row, $tpl['cf_url']), $row);
+	$tpl['projects'][$row['id']] = $row;
+}
+
+asort($lng['cf_category']);
+
+$tpl['currency_list'] = get_currency_list($db);
+
+if (isset($_REQUEST['parameters']['category_id']))
+	$tpl['cur_category'] = $lng['cf_category'][$tpl['category_id']];
+else
+	$tpl['cur_category'] = false;
+
+require_once( ABSPATH . 'templates/cf_catalog.tpl' );
+
+?>
