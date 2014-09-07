5c18319892e43a6243cf81bd66dd941d95d157f3
root <you@example.com>
2014-09-07 13:06:41 +0400

    content/del_credit.php

--- content/del_credit.php
+++ content/del_credit.php
@@ -1,15 +1,13 @@
 <?php
 if (!defined('DC')) die("!defined('DC')");
 
-$tpl['data']['type'] = 'change_creditor';
+$tpl['data']['type'] = 'del_credit';
 $tpl['data']['type_id'] = ParseData::findType($tpl['data']['type']);
 $tpl['data']['time'] = time();
 $tpl['data']['user_id'] = $user_id;
 
-$tpl['variables'] = ParseData::get_variables ($db,  array('limit_change_host', 'limit_change_host_period') );
-
 $tpl['credit_id'] = intval($_REQUEST['parameters']['credit_id']);
 
-require_once( ABSPATH . 'templates/change_creditor.tpl' );
+require_once( ABSPATH . 'templates/del_credit.tpl' );
 
 ?>
