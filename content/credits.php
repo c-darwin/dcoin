2898e4cb6fb98b147fee55cf447b226d75de6809
root <you@example.com>
2014-09-07 13:06:41 +0400

    content/credits.php

--- content/credits.php
+++ content/credits.php
@@ -1,73 +1,32 @@
 <?php
 if (!defined('DC')) die("!defined('DC')");
 
-$res = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, '
+$tpl['data']['credit_part_type'] = 'change_credit_part';
+$tpl['data']['credit_part_type_id'] = ParseData::findType($tpl['data']['credit_part_type']);
+$tpl['data']['time'] = time();
+$tpl['data']['user_id'] = $user_id;
+
+$res = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
 		SELECT *
-		FROM `'.DB_PREFIX.'daemons`
-		ORDER BY `script`
-		');
+		FROM `".DB_PREFIX."credits`
+		WHERE (`from_user_id` = {$user_id} OR `to_user_id` = {$user_id}) AND
+					 `del_block_id` = 0
+		");
 while ($row = $db->fetchArray($res)) {
-	$row['time'] = date('d-m-Y H:i:s', $row['time']);
-	$row['memory'] = round($row['memory']/1024/1024, 2);
-	$tpl['demons'][] = $row;
+	$row['time'] = date('d/m/Y H:i:s', $row['time']);
+	if ($user_id == $row['from_user_id'])
+		$tpl['I_debtor'][] = $row;
+	else
+		$tpl['I_creditor'][] = $row;
 }
 
-$res = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, '
-		SELECT `'.DB_PREFIX.'nodes_ban`.`ban_start`,
-					  `'.DB_PREFIX.'nodes_ban`.`user_id`,
-					  `'.DB_PREFIX.'miners_data`.`host`,
-					  `'.DB_PREFIX.'nodes_ban`.`info`
-		FROM `'.DB_PREFIX.'nodes_ban`
-		LEFT JOIN `'.DB_PREFIX.'miners_data` ON `'.DB_PREFIX.'miners_data`.`user_id` = `'.DB_PREFIX.'nodes_ban`.`user_id`
-		ORDER BY `ban_start`
-		');
-while ($row = $db->fetchArray($res)) {
-	$row['ban_start'] = date('d-m-Y H:i:s', $row['ban_start']);
-	$tpl['nodes_ban'][] = $row;
-}
+$tpl['credit_part'] = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
+		SELECT `credit_part`
+		FROM `".DB_PREFIX."users`
+		WHERE `user_id` = {$user_id}
+		", 'fetch_one');
 
+$tpl['currency_list'] = get_currency_list($db);
 
-$res = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, '
-		SELECT *
-		FROM `'.DB_PREFIX.'nodes_connection`
-		ORDER BY `user_id`
-		');
-while ($row = $db->fetchArray($res)) {
-	$tpl['nodes_connection'][] = $row;
-}
-
-$res = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, '
-		SELECT *
-		FROM `'.DB_PREFIX.'main_lock`
-		');
-while ($row = $db->fetchArray($res)) {
-	$row['lock_time'] = date('d-m-Y H:i:s', $row['lock_time']);
-	$tpl['main_lock'][] = $row;
-}
-
-$tpl['queue_tx'] = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, '
-		SELECT count(*)
-		FROM `'.DB_PREFIX.'queue_tx`
-		', 'fetch_one');
-
-$tpl['transactions_testblock'] = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, '
-		SELECT count(*)
-		FROM `'.DB_PREFIX.'transactions_testblock`
-		', 'fetch_one');
-
-$tpl['transactions'] = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, '
-		SELECT count(*)
-		FROM `'.DB_PREFIX.'transactions`
-		', 'fetch_one');
-
-$res = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, '
-		SELECT `name`, `value`
-		FROM `'.DB_PREFIX.'variables`
-		');
-while ($row = $db->fetchArray($res)) {
-	$tpl['variables'][$row['name']] = $row['value'];
-}
-
-require_once( ABSPATH . 'templates/db_info.tpl' );
-
+require_once( ABSPATH . 'templates/credits.tpl' );
 ?>
