10020a75e6fdaa8fd5e1f58a7b2b81b3a1d0544d
Darwin <darwin84@yandex.ru>
2014-09-13 13:44:17 +0400

    $_REQUEST[id]

--- get_block.php
+++ get_block.php
@@ -13,17 +13,19 @@ require_once( ABSPATH . 'includes/class-mysql.php' );
 
 $db = new MySQLidb(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
 
-if (check_input_data($_REQUEST['id'], 'int') ) {
+if (isset($_REQUEST['id']))
+	$id = intval($_REQUEST['id']);
+if ($id) {
 
 	if (isset($_REQUEST['download'])) {
 		header('Content-type: application/octet-stream');
-		header('Content-Disposition: attachment; filename="'.$_REQUEST['id'].'"');
+		header('Content-Disposition: attachment; filename="'.$id.'"');
 	}
 
 	$block = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, "
 			SELECT `data`
 			FROM `".DB_PREFIX."block_chain`
-			WHERE `id` = {$_REQUEST['id']}
+			WHERE `id` = {$id}
 			", 'fetch_one' );
 	echo $block;
 }
