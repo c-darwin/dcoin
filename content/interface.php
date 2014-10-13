b78d28eb83e1f3dcbdc50fe5b02b9b94134754e5
Darwin <darwin84@yandex.ru>
2014-10-13 13:00:39 +0400

    Available for restricted

--- content/interface.php
+++ content/interface.php
@@ -1,10 +1,13 @@
 <?php
 if (!defined('DC')) die("!defined('DC')");
 
+if (!empty($_SESSION['restricted']))
+	exit;
+
 $tpl['show_sign_data'] = $db->query( __FILE__, __LINE__,  __FUNCTION__,  __CLASS__, __METHOD__, '
-		SELECT `show_sign_data`
-		FROM `'.DB_PREFIX.MY_PREFIX.'my_table`
-		', 'fetch_one');
+			SELECT `show_sign_data`
+			FROM `'.DB_PREFIX.MY_PREFIX.'my_table`
+			', 'fetch_one');
 
 require_once( ABSPATH . 'templates/interface.tpl' );
 
