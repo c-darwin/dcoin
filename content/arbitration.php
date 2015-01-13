0cf5e0baf364647dcea5f03e67788e8b1438d094
Darwin <darwin84@yandex.ru>
2015-01-13 15:59:59 +0300

    added pending tx

--- content/arbitration.php
+++ content/arbitration.php
@@ -83,7 +83,7 @@ $tpl['last_tx'] = get_last_tx($user_id, types_to_ids(array('change_arbitrator_co
 if (!empty($tpl['last_tx']))
 	$tpl['last_tx_formatted'] = make_last_txs($tpl['last_tx']);
 
-$tpl['pending_tx'] = $pending_tx[ParseData::findType('change_arbitrator_list')];
+$tpl['pending_tx'] = @$pending_tx[ParseData::findType('change_arbitrator_list')];
 
 require_once( ABSPATH . 'templates/arbitration.tpl' );
 ?>
